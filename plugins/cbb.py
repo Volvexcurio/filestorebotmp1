f5f4fd646c6d4be9e6c1cc3501ce76f79d50dc1e
Volvexcurio <87892543+Volvexcurio@users.noreply.github.com>
2021-08-14 18:13:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={Curio_VolvEX}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/joinchat/Od06qI9XIFk0ZGVl'>Click here</a>\n○ Channel : @MoviePlayr\n○ Support Channel : @MoviePlayr</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={@Curio_VolvEX}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/joinchat/Od06qI9XIFk0ZGVl'>Click here</a>\n○ Channel : @MoviePlayr\n○ Support Channel : @MoviePlayr</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
